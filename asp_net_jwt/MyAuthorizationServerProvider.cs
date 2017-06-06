f080939def1a488fe560b9caa6c1d95be53a3607
anucha8p <anucha8p@gmail.com>
2017-06-06 10:48:01 +0700

    add note

---
+++ asp_net_jwt/MyAuthorizationServerProvider.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Owin.Security.OAuth;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace TestJwt
+{
+    public class MyAuthorizationServerProvider : OAuthAuthorizationServerProvider
+    {
+        public override Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+        {
+            context.Validated();
+
+            return Task.FromResult<object>(null);
+        }
+
+        public override Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
+        {
+            var identity = new ClaimsIdentity(context.Options.AuthenticationType);
+            if (context.UserName == "admin" && context.Password == "admin")
+            {
+                identity.AddClaim(new Claim(ClaimTypes.Role, "admin"));
+                identity.AddClaim(new Claim("username", "admin"));
+                identity.AddClaim(new Claim(ClaimTypes.Name, "Anucha Patthanajan"));
+                context.Validated(identity);
+            }
+            else if (context.UserName == "user" && context.Password == "user")
+            {
+                identity.AddClaim(new Claim(ClaimTypes.Role, "user"));
+                identity.AddClaim(new Claim("username", "user"));
+                identity.AddClaim(new Claim(ClaimTypes.Name, "Patcharanan Thaput"));
+                context.Validated(identity);
+            }
+            else
+            {
+                context.SetError("invalid_grant", "Provided username and password is incorrect");
+            }
+
+            return Task.FromResult<object>(null);
+        }
+    }
+}
---
+++ asp_net_jwt/Startup.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Owin;
+using Microsoft.Owin.Security.OAuth;
+using Owin;
+using System;
+using System.Web.Http;
+
+[assembly: OwinStartup(typeof(TestJwt.Startup))]
+
+namespace TestJwt
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            var myProvider = new MyAuthorizationServerProvider();
+            var options = new OAuthAuthorizationServerOptions
+            {
+                AllowInsecureHttp = true,
+                TokenEndpointPath = new PathString("/Token"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromDays(1),
+                Provider = myProvider
+            };
+            app.UseOAuthAuthorizationServer(options);
+            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
+
+            var config = new HttpConfiguration();
+            WebApiConfig.Register(config);
+        }
+    }
+}
